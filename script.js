549112dab2f559a3aa5eac9d068c819c6dbfa797
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-13 23:54:53 -0300

    Add

---
+++ script.js
@@ -0,0 +1,8 @@
+function contactWindow(){
+    let x = document.getElementById("conWin")
+    if (x.style.display === "none"){
+        x.style.display = "block"
+    } else{
+        x.style.display = "none"
+    }
+}
